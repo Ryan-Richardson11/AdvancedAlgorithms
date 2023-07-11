c88dcc06f3ce8b89dfbba4de519070563fe438b7
Ryan Richardson <ryanrich811@gmail.com>
2023-07-11 12:01:14 -0400

    Updated range and docstring for generate_vector

--- Week2/Worksheet2.py
+++ Week2/Worksheet2.py
@@ -3,14 +3,16 @@ import random
 def generate_vector():
     """
     generate_vector(): 
-    Creates an empty list then appends a random number from -500 to 500, 1,000,000 times. The vector is then returned.
+    Creates an empty list then appends a random number from 1 to 1,000,000, 1,000 times. The vector is then returned.
     """
     vector = []
-    for i in range(0, 1_000_000):
-        vector.append(random.randint(-500, 500))
+    for i in range(0, 1000):
+        vector.append(random.randint(1, 1_000_000))
     return vector
 
 def is_unique(n):
+    """
+    """
     for i in range(len(n)):
         for j in range(i+1, len(n)):
             if n[i] == n[j]:
@@ -19,6 +21,7 @@ def is_unique(n):
             
 def main():
     v = generate_vector()
+    print(v)
     print(is_unique(v))
     
 main()
