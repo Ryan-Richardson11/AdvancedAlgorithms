4bf9ff4c6b0194bc6eed5b5c7bbcc617268cc2f3
Ryan Richardson <ryanrich811@gmail.com>
2023-07-11 16:00:09 -0400

    implemented brute force algorithm for finding the mode

--- Week2/Worksheet2.2.py
+++ Week2/Worksheet2.2.py
@@ -1,4 +1,13 @@
-arr = []
+a = []
 
-def find_mode():
-    pass
+def find_mode(a):
+    max_value, max_count = 0, 0
+    for i in range(len(a)):
+        count = 0
+        for j in range(len(a)):
+            if (a[i] == a[j]):
+                count += 1
+        if (count > max_count):
+            max_count = count
+            max_value = a[i]
+    return max_value
