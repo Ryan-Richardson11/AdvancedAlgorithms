a9a2c8578fd4943c8eca99de484052c2eea6ca21
Ryan Richardson <ryanrich811@gmail.com>
2023-07-11 12:06:45 -0400

    first implementation of transform and conquer algorithm

--- Week2/Worksheet2.py
+++ Week2/Worksheet2.py
@@ -10,7 +10,7 @@ def generate_vector():
         vector.append(random.randint(1, 1_000_000))
     return vector
 
-def is_unique(n):
+def is_unique_brute(n):
     """
     is_unique(): Uses brute for to iterate through the vector and check if any of the values match.
     Takes 1 vector as an argument and returns Not Unique or Unique.
@@ -21,12 +21,23 @@ def is_unique(n):
                 return "Not Unique"
     return "Unqiue"
             
+def is_unique_trans(n):
+    """
+    is_unique(): Uses transform and conquer algorithm to check if any of the values match.
+    Takes 1 vector as an argument and returns Not Unique or Unique.
+    """
+    for i in range(len(n)):
+        for j in range(i+1, len(n)):
+            if n[i] == n[j]:
+                return "Not Unique"
+    return "Unqiue"
+
 def main():
     """
     main(): Stores a generated vector in the variable v, then prints it and passes it through is_unique as an argument.
     """
     v = generate_vector()
     print(v)
-    print(is_unique(v))
+    print(is_unique_brute(v))
     
 main()
