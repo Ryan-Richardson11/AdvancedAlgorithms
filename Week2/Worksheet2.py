9d60d2e26d9de7dfdd4ea23ddf2b4525b6a8754a
Ryan Richardson <ryanrich811@gmail.com>
2023-07-11 12:38:25 -0400

    Updated both unique algorithms to match classwork

--- Week2/Worksheet2.py
+++ Week2/Worksheet2.py
@@ -10,34 +10,39 @@ def generate_vector():
         vector.append(random.randint(1, 1_000_000))
     return vector
 
-def is_unique_brute(n):
+def is_unique_brute(a):
     """
     is_unique(): Uses brute for to iterate through the vector and check if any of the values match.
     Takes 1 vector as an argument and returns Not Unique or Unique.
     """
-    for i in range(len(n)):
-        for j in range(i+1, len(n)):
-            if n[i] == n[j]:
-                return "Not Unique"
-    return "Unqiue"
+    ans = True
+    for i in range(len(a)):
+        for j in range(len(a)):
+            if (i != j) and (a[i] == a[j]):
+                ans = False
+                break
+        if (not ans):
+            break
+    return ans
             
-def is_unique_trans(n):
+def is_unique_trans(a):
     """
     is_unique(): Uses transform and conquer algorithm to check if any of the values match.
     Takes 1 vector as an argument and returns Not Unique or Unique.
     """
-    for i in range(len(n)):
-        for j in range(i+1, len(n)):
-            if n[i] == n[j]:
-                return "Not Unique"
-    return "Unqiue"
+    a.sort()
+    for i in range(len(a)):
+        if (a[i-1] == a[i]):
+            return False
+    return True
 
 def main():
     """
-    main(): Stores a generated vector in the variable v, then prints it and passes it through is_unique as an argument.
+    main(): Stores a generated vector in the variable v, then prints it and passes it through is_unique methods as an argument.
     """
     v = generate_vector()
     print(v)
     print(is_unique_brute(v))
+    print(is_unique_trans(v))
     
 main()
