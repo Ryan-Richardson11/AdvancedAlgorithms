e6a84c2524c89bb6712b65785accf178f98e0d24
Ryan Richardson <ryanrich811@gmail.com>
2023-07-11 16:29:23 -0400

    New Algorithm using count() method for non brute force

--- Week2/Worksheet2.2.py
+++ Week2/Worksheet2.2.py
@@ -1,6 +1,4 @@
-a = []
-
-def find_mode(a):
+def find_mode_brute(a):
     max_value, max_count = 0, 0
     for i in range(len(a)):
         count = 0
@@ -11,3 +9,20 @@ def find_mode(a):
             max_count = count
             max_value = a[i]
     return max_value
+
+def find_mode(a):
+    mode = None
+    max_count = 0
+
+    for i in range(len(a)):
+        counter = a.count(a[i])
+        if counter > max_count:
+            max_count = counter
+            mode = a[i]
+    return mode
+
+def main():
+    a = [3, 3, 3, 2, 5, 76, 734, 23, 5, 5, 4, 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4]
+    print(find_mode_brute(a))
+    print(find_mode(a))
+main()
