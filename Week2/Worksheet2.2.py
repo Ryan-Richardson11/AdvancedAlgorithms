72a89d5c152099d20074b66faaf66dd11caa4569
Ryan Richardson <ryanrich811@gmail.com>
2023-07-12 09:05:54 -0400

    Changed worksheet 2.2 algorithm and added documentation

--- Week2/Worksheet2.2.py
+++ Week2/Worksheet2.2.py
@@ -10,53 +10,35 @@ def generate_array():
         a.append(random.randint(0, 9))
     return a
 
-def find_mode_brute(a):
-    max_value, max_count = 0, 0
-    for i in range(len(a)):
-        count = 0
-        for j in range(len(a)):
-            if (a[i] == a[j]):
-                count += 1
-        if (count > max_count):
-            max_count = count
-            max_value = a[i]
-    return max_value
 
 def find_mode(a):
-    mode = None
+    """"
+    find_mode(): Uses transform and conquer by first sorting the array then iterating through each sequence and adding to a counter.
+    Passes an array through as an argument.
+    Accounts for multiple modes by appending to a list.
+    """
+    mode = []
     max_count = 0
     a.sort()
 
-    count = 0
+    count = 1
     for i in range(len(a)):
         if i < len(a) - 1 and a[i] == a[i+1]:
             count += 1
-        else:
-            count = 0
-        if count > max_count:
-            max_count = count
-            mode = [a[i]]
-        elif count == max_count:
-            mode.append(a[i])
+        else:  
+            if count > max_count:
+                max_count = count
+                mode = [a[i]]
+            elif count == max_count:
+                mode.append(a[i])
+            count = 1
     return mode
 
-# def find_mode_set(a):
-#     mode = None
-#     max_count = 0
-#     distinct_values = set(a)
-
-#     for value in distinct_values:
-#         counter = a.count(value)
-#         if counter > max_count:
-#             max_count = counter
-#             mode = value
-
-#     return mode
-
 def main():
+    """
+    main(): Calls generate_array to create a random array and passes it through find_mode() to calculate the mode of the array.
+    """
     a = generate_array()
-
-    print(find_mode_brute(a))
     print(find_mode(a))
 
 main()
