38ffddfdb0c45cbc421889994d39a1818756debf
Ryan Richardson <ryanrich811@gmail.com>
2023-07-19 09:20:50 -0400

    First implementation of tribonacci, No test cases done

--- Week3/Project3.py
+++ Week3/Project3.py
@@ -1,2 +1,11 @@
 def tribo(n):
     a, b, c = 1, 1, 1
+    if (n < 4):
+        return 1
+    for i in range(3, n):
+        a, b, c = c, b + c, a + b
+    return c
+
+def main():
+    n = int(eval(input("Choose an Integer: ")))
+    print(f"The {n}-th element of Fibonacci is {tribo(n)}")
