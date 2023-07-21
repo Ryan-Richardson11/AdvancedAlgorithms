628ae50bc12febe4d57587b5c31b3d744b89c77e
Ryan Richardson <ryanrich811@gmail.com>
2023-07-21 09:44:30 -0400

    Documentation for project 03

--- Week3/Project3.py
+++ Week3/Project3.py
@@ -1,4 +1,8 @@
 def tribo(n):
+    """
+    tribo(n): Efficient dynamic programmming algorithms to solve fibonacci but adding the previous 3 integers instead of two.
+    Takes an input n to find the nth elements Tribonacci number.
+    """
     a, b, c = 1, 1, 1
     if (n < 4):
         return 1
@@ -7,6 +11,10 @@ def tribo(n):
     return c
 
 def main():
+        """
+        main(): Continually asks for a positive integer in a while loop and uses that as n to call tribo().
+        Try-except loop to catch any input errors.
+        """
         while True:
             try:
                 n = int(eval(input("Choose a Positive Integer: ")))
