55d2020026213e31bec1b63e4ffbb911a6d0a3ea
unknown <ryanrich811@gmail.com>
2023-07-24 13:29:22 -0400

    Added hardcoded values for function call

--- Week4/Worksheet4.py
+++ Week4/Worksheet4.py
@@ -16,11 +16,20 @@ def egyptian(n, d):
 
 def main():
     print("Greedy Algorithm to Compute Egytian Fractions")
-    stay = True
-    while stay:
-        n = int(input("Enter the numerator: "))
-        d = int(input("Enter the denominator: "))
-        egyptian(n, d)
-        stay = ("n" != (input("\nCompute another? (no to stop) ")+" ")[0])
+    # stay = True
+    # while stay:
+    #     n = int(input("Enter the numerator: "))
+    #     d = int(input("Enter the denominator: "))
+    #     egyptian(n, d)
+    #     stay = ("n" != (input("\nCompute another? (no to stop) ")+" ")[0])
+    egyptian(5, 6)
+    print("\n")
+    egyptian(7, 15)
+    print("\n")
+    egyptian(23, 34)
+    print("\n")
+    egyptian(121, 321)
+    print("\n")
+    egyptian(5, 123)
 
 main()
