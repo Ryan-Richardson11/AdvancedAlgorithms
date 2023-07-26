fc0f0004fc7a4d52a3b3ed8faaf645f522f29abb
Ryan Richardson <ryanrich811@gmail.com>
2023-07-26 17:03:00 -0400

    Functional code for reading file and finding cubic volume

--- Week4/Project4.py
+++ Week4/Project4.py
@@ -1,38 +1,36 @@
-# knapsack unbounded - Greedy approach
+# # knapsack unbounded - Greedy approach
 
-def knapsack(v, w, cap):
+def knapsack(name, value, height, width, depth, cap):
     rwv = []         # triplet ratio, weight, value, index
-    for i in range(len(v)):
-        rwv.append([v[i]/w[i],w[i],v[i],i])
+    for i in range(len(value)):
+        volume = height[i] * width[i] * depth[i]
+        rwv.append([name[i], value[i] / volume, volume, value[i], i])
     rwv.sort(reverse=True)    # sort from high to low rate
+    print(rwv)
     ans = []                     # the list of added items
-    tw = 0                                  # total weight
+    tv = 0                                  # total weight
     found = True
-    while (found):        # until no fitting item is found
+    while found:        # until no fitting item is found
         found = False
         for t in rwv:              # search an item to add
-            if (t[1] + tw) <= cap:      # if the item fits
-                ans.append(t[3])                  # add it
-                tw += t[1]
+            if (t[2] + tv) <= cap:      # if the item fits
+                ans.append(t[0])                  # add it
+                tv += t[2]
                 found = True
                 break
     return ans           # returns the list of added items
 
 def main():
-    items = int(input("Number of distinct items: "))
-    values, weights = [],[]
-    for i in range(items):
-        v = int(input("Value of item "+str(i+1)+": "))
-        w = int(input("Weight of item "+str(i+1)+": "))
-        values.append(v)
-        weights.append(w)
-    capacity = int(input("Maximum weight (capacity): "))
-    answer = knapsack(values, weights, capacity)
-    tv, tw = 0, 0
-    for a in answer:
-        print("Item - Value:", values[a], "- Weight:", weights[a])
-        tv += values[a]
-        tw += weights[a]
-    print("Items:", len(answer), "- Value:", tv, "- Weight:", tw)
-
+    my_file = open("C:\\Users\\Ryan\\Desktop\\AdvancedAlgorithms\Week4\\Project4.csv", "r")
+    name, value, height, width, depth = [], [], [], [], []
+    for line in my_file:
+        info = line.strip().split(",")
+        name.append(info[0])
+        value.append(int(info[1]))
+        height.append(int(info[2]))
+        width.append(int(info[3]))
+        depth.append(int(info[4]))
+    
+    print(knapsack(name, value, height, width, depth, 100))
+    
 main()
