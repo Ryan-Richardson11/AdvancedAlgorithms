317ee4c6497421d9f603fffdb6a975189a7af62e
unknown <ryanrich811@gmail.com>
2023-07-26 11:11:08 -0400

    Hardcoded values and weights for the knapsack problem

--- Week4/Worksheet4.3.py
+++ Week4/Worksheet4.3.py
@@ -19,20 +19,19 @@ def knapsack(v, w, cap):
     return ans           # returns the list of added items
 
 def main():
-    # items = int(input("Number of distinct items: "))
-    # values, weights = [],[]
-    # for i in range(items):
-    #     v = int(input("Value of item "+str(i+1)+": "))
-    #     w = int(input("Weight of item "+str(i+1)+": "))
-    #     values.append(v)
-    #     weights.append(w)
-    # capacity = int(input("Maximum weight (capacity): "))
-    # answer = knapsack(values, weights, capacity)
-    # tv, tw = 0, 0
-    # for a in answer:
-    #     print("Item - Value:", values[a], "- Weight:", weights[a])
-    #     tv += values[a]
-    #     tw += weights[a]
-    # print("Items:", len(answer), "- Value:", tv, "- Weight:", tw)
-    print(knapsack(5, 10, 838))
+    values1 = [5, 8, 12]
+    weights1 = [10, 20, 30]
+    print(knapsack(values1, weights1, 838))
+    print("\n")
+    values2 = [5, 5, 7, 11, 13]
+    weights2 = [17, 23, 29, 31, 37]
+    print(knapsack(values2, weights2, 997))
+    print("\n")
+    values3 = [5, 6, 7, 8]
+    weights3 = [25, 36, 49, 64]
+    print(knapsack(values3, weights3, 250))
+    print("\n")
+    values4 = [5, 6, 7, 8]
+    weights4 = [25, 36, 49, 64]
+    print(knapsack(values4, weights4, 360))
 main()
