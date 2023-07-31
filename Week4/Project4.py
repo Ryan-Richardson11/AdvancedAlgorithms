a438f6fe7104bbb1aa0cc3966be7cd12fa8afe56
Ryan Richardson <ryanrich811@gmail.com>
2023-07-31 16:56:12 -0400

    Handles multiple items and counts

--- Week4/Project4.py
+++ Week4/Project4.py
@@ -1,18 +1,18 @@
 # # knapsack unbounded - Greedy approach
 
 def knapsack(name, value, height, width, depth, cap):
-    rwv = []         # triplet ratio, weight, value, index
+    rvv = []         # triplet ratio, volume, value, index
     for i in range(len(value)):
         # Change to a function
         volume = height[i] * width[i] * depth[i] 
-        rwv.append([name[i], value[i] / volume, volume, value[i], i])
-    rwv.sort(reverse=True)    # sort from high to low rate
+        rvv.append([name[i], value[i] / volume, volume, value[i], i])
+    rvv.sort(reverse=True)    # sort from high to low rate
     ans = []                     # the list of added items
-    tv = 0                                  # total weight
+    tv = 0                                  # total volume
     found = True
     while found:        # until no fitting item is found
         found = False
-        for t in rwv:              # search an item to add
+        for t in rvv:              # search an item to add
             if (t[2] + tv) <= cap:      # if the item fits
                 ans.append(t[0])                  # add it
                 tv += t[2]
@@ -21,7 +21,10 @@ def knapsack(name, value, height, width, depth, cap):
     return ans           # returns the list of added items
 
 def main():
-    my_file = open("C:\\Users\\Ryan\\Desktop\\AdvancedAlgorithms\Week4\\Project4.csv", "r", encoding="utf-8-sig")
+    """
+    Reads lines from a csv file and appends each to a list
+    """
+    my_file = open("C:\\Users\\Ryan\\Desktop\\AdvancedAlgorithms\Week4\\packs.csv", "r", encoding="utf-8-sig")
     name, value, height, width, depth = [], [], [], [], []
     for line in my_file:
         info = line.strip().split(",")
@@ -37,12 +40,15 @@ def main():
         else:
             answer = knapsack(name, value, height, width, depth, capacity)
             tvol, tval = 0, 0
+            item_counts = {item: answer.count(item) for item in set(answer)}
+            items_str = ", ".join([f"{count} {item}" for item, count in item_counts.items()])
             for a in answer:
                 idx = name.index(a)
                 volume = height[idx] * width[idx] * depth[idx]
                 print(f"Item: {name[idx]} Value: {value[idx]} - Volume: {volume}")
                 tval += value[idx]
                 tvol += volume
-            print(f"The suggested items are {len(answer)} {name[idx]}'s with a total value of ${tval}. There were {capacity - tvol} square inches left unused.")
+            print(f"The suggested items are: {items_str} with a total value of ${tval}. There were {capacity - tvol} square inches left unused.")
     
 main()
+
