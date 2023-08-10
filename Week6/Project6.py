124761490aadf48f1a1e4ac2cee7b4cd2ad0a2f5
Ryan Richardson <ryanrich811@gmail.com>
2023-08-10 17:43:02 -0400

    Added inputs for arguments to numpy calls

--- Week6/Project6.py
+++ Week6/Project6.py
@@ -1,8 +1,13 @@
 import numpy as np
 
 
+constraint_limits = eval(input("Please enter an array of constraints (ex. [[2,4,5], [1,2,4], [8,0,3]]): "))
+A = np.array(constraint_limits)
 
-A = np.array()
+
 inv_A = np.linalg.inv(A)
+
+constraints = eval(input("Please enter the constraints for the problem (ex. [300, 200, 200]): "))
 b = np.array()
-x = np.linalg.inv.dot(b)
+
+x = np.linalg.inv(A).dot(b)
