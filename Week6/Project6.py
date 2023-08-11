6833967a471fd20637a89c3a8b5f0045160c618b
Ryan Richardson <ryanrich811@gmail.com>
2023-08-11 11:11:32 -0400

    Progression on print statements for table output

--- Week6/Project6.py
+++ Week6/Project6.py
@@ -1,17 +1,40 @@
 import numpy as np
 
-variables = int(input("Please enter the number of variables: "))
-
-constraints = eval(input("Enter the data for the square matrix using stating the constraints (ex. [[2,4,5], [1,2,4], [8,0,3]]): "))
-A = np.array(constraints)
-
-inv_A = np.linalg.inv(A)
-
-coefficients = eval(input("Enter the coefficients of each variable for the objective function (ex. [300, 200, 200]): "))
-b = np.array(coefficients)
-
-x = np.linalg.inv(A).dot(b)
 
 
-print(inv_A)
-print(x)
+
+
+
+def main():
+
+    variables = int(input("Please enter the number of variables: "))
+    matrix = eval(input("Enter the data for the square matrix using stating the constraints (ex. [[2,4,5], [1,2,4], [8,0,3]]): "))
+    constraint_limits = eval(input("Enter the constraint limits (ex. [300, 200, 300]): "))
+    coeffiecients = input("Enter the coefficients of each variable for the objective function (ex. 3000, 2000, 2000): ")
+
+    variables = 3
+    matrix = [[2,4,5], [1,2,4], [8,0,3]]
+    constraint_limits = [300, 200, 300]
+    coeffiecients = 3000, 2000, 2000
+
+
+
+    # A = np.array(matrix)
+    A = np.array([[2,4,5], [1,2,4], [8,0,3]])
+    # inv_A = np.linalg.inv(A)
+    inv_A = np.linalg.inv(A)
+    # b = np.array(constraint_limits)
+    b = np.array([300, 200, 300])
+    # x = np.linalg.inv(A).dot(b)
+    x = np.linalg.inv(A).dot(b)
+
+    for i in range(variables):
+        for j in matrix:
+            for k in coeffiecients:
+                print(f"Variable {i}, {j}, {k}")
+    
+    print(f"AVAILIBILITY, {constraint_limits}")
+    
+    # print(x)
+
+main()
