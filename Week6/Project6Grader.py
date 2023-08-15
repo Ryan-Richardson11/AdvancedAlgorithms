92787cd0c003d579fbf74cdaf5bcb7b132095925
Ryan Richardson <ryanrich811@gmail.com>
2023-08-15 10:37:44 -0400

    Starting documentation for functions

--- Week6/Project6Grader.py
+++ Week6/Project6Grader.py
@@ -1,6 +1,9 @@
 import numpy as np
 
 def best_combination(matrix):
+    """
+    best_combination(): Returns converted matrix from one that prints out the table to one that can be used in numpy.
+    """
     adjusted_matrix = []
     
     for i in range(len(matrix[0])):
@@ -10,6 +13,11 @@ def best_combination(matrix):
     return adjusted_matrix
 
 def find_units(array, constraint_limits):
+    """
+    find_units(): Finds the number of units by finding the limiting ratio of each subarray to its constraint, 
+    then divides the constraint by the value in that index to get units.
+    Parameters: An array and its constraints.
+    """
     ratio_arr = [array[i] / constraint_limits[i] for i in range(len(array))]
     limit_ratio = max(ratio_arr)
     limit_index = ratio_arr.index(limit_ratio)
