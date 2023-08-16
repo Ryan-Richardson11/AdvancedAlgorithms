81e008ef873206957a4b7fcf91449089b0b67324
unknown <ryanrich811@gmail.com>
2023-08-16 14:01:18 -0400

    Added a counter to the Las Vegas algorithm to track number of tries

--- Week7/Worksheet1.py
+++ Week7/Worksheet1.py
@@ -7,15 +7,17 @@ def generate_array():
     return arr
 
 def find_one(arr):
+    count = 0
     while True:
         idx = random.randint(0,10_000)
+        count += 1
         if arr[idx] == 1:
-            print(f"1 was found at index {idx}")
+            print(f"1 was found at index {idx} after {count} tries")
             break
 
 def main():
 
     arr = generate_array()
     find_one(arr)
-    
+
 main()
