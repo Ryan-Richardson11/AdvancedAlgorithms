8290d9f2c7f8198502ad6b4b314ff01203ce4768
unknown <ryanrich811@gmail.com>
2023-08-18 14:29:17 -0400

    changed assertEquals unit test

--- Week7/Project7UnitTests.py
+++ Week7/Project7UnitTests.py
@@ -4,8 +4,7 @@ import unittest
 class TestHillClimb(unittest.TestCase):
 
     def returns_local_value(self):
-        hillClimb([6,5,5,5,4,3,2], 5)
-        self.assertEqual(6)
+        self.assertEqual(hillClimb([6,5,5,5,4,3,2], 5),6)
 
 
     if __name__ == "__main__":
