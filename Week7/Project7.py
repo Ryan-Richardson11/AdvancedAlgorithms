f683494c5c2a45e0ebdd63e5b33ce38eb9c2f4f2
Ryan Richardson <ryanrich811@gmail.com>
2023-08-22 19:52:45 -0400

    Added documentation to project for each function

--- Week7/Project7.py
+++ Week7/Project7.py
@@ -3,6 +3,10 @@ import math
 log2 = math.log2
 
 def myFunction(x):
+    """
+    Psuedorandom number generator to insert into an array.
+    Parameters: A number, in this case through a range.
+    """
     if (x == 0):
         return 0
     elif ((log2(x) * 7) % 17) < (x % 13):
@@ -13,9 +17,18 @@ def myFunction(x):
         return (log2(x)**2) - x
 
 def randomIndex():
+    """
+    Returns a random index between 1 and 9998 as the starting point.
+    Avoids edge cases for the random starting point.
+    """
     return random.randint(1, 9998)
     
 def hillClimb(arr, start_index):
+    """
+    Uses while loops to check if the number to the right or left is bigger and finds
+    the local maximum. Will traverse shoulders starting right first.
+    Parameters: An array and random starting index.
+    """
     if start_index == 0 or start_index == len(arr) - 1:
         return arr[start_index]
     
@@ -29,6 +42,10 @@ def hillClimb(arr, start_index):
         return start_index, arr[start_index]
 
 def main():
+    """
+    main(): Calls my function in range 10000 to get the random generated array. Then calls randomIndex()
+    to use as parameters for hillClimb().
+    """
     arr = [myFunction(i) for i in range(0, 10000)]
     idx = randomIndex()
     print(f"Random starting index: {idx}")
